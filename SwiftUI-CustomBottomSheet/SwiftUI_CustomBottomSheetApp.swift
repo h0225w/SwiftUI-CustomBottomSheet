44d10b502b337062e894309f9851056ec87df68c
HyeonWoo Park <gusdn5387@naver.com>
2024-07-27 16:35:23 +0900

    Initial Commit

---
+++ SwiftUI-CustomBottomSheet/SwiftUI_CustomBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CustomBottomSheetApp.swift
+//  SwiftUI-CustomBottomSheet
+//
+//  Created by 박현우 on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CustomBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
